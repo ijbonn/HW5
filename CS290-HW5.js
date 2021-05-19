905193d048827d38890b051ada60d6494b95932f
ijbonn <66021174+ijbonn@users.noreply.github.com>
2021-05-18 23:12:13 -0600

    Update CS290-HW5.js

--- CS290-HW5.js
+++ CS290-HW5.js
@@ -32,3 +32,7 @@ app.post('/', function(req,res){
   context.dataList = qParams;
   res.render('home', context);
 });
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
