72aabbd8ca218a8dbe18dcff69bae9399d524326
ijbonn <66021174+ijbonn@users.noreply.github.com>
2021-05-18 23:04:24 -0600

    Create CS290-HW5.js

---
+++ CS290-HW5.js
@@ -0,0 +1,34 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 4491);
+
+app.get('/',function(req,res){
+  var qParams = [];
+  for (var p in req.query){
+    qParams.push({'name':p,'value':req.query[p]})
+  }
+  var context = {};
+  context.reqType = 'GET';
+  context.dataList = qParams;
+  res.render('home', context);
+});
+
+app.post('/', function(req,res){
+  var qParams = [];
+  for (var p in req.body){
+    qParams.push({'name':p,'value':req.body[p]})
+  }
+  var context = {};
+  context.reqType = 'POST';
+  context.dataList = qParams;
+  res.render('home', context);
+});
